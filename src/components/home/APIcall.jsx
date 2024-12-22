084c07bf4cf6897ea6d4f0aa1b1ca3c29cf9cdc7
tai tran <taitran@sandiego.edu>
2024-12-21 21:42:47 -0800

    Hot collection: API calls, carousel, skeleton loading

---
+++ src/components/home/APIcall.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import HotCollections from './HotCollections';
+
+const APIcall = () => {
+    const [nft, setNft] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const fetchAPI = async () => {
+        setLoading(true);
+
+        const { data } = await axios.get('https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections');
+
+        setNft(data);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        fetchAPI();
+    }, []);
+
+    return (
+        <>
+            <HotCollections nft={nft} loading={loading} />
+        </>
+    );
+}
+
+export default APIcall
