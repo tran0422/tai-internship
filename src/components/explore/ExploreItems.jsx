6fc5286cf5e5947f8abb0f520b11295f22e41a05
tai tran <taitran@sandiego.edu>
2024-12-27 22:36:47 -0800

    removed commented out lines that are not needed.

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-// import AuthorImage from "../../images/author_thumbnail.jpg";
-// import nftImage from "../../images/nftImage.jpg";
 import Countdown from '../home/Countdown';
 
 const ExploreItems = ({ nft }) => {
@@ -16,15 +14,6 @@ const ExploreItems = ({ nft }) => {
   const renderNft = nft.slice(0, visibleItems).map((collection, index) => {
 
     return (
-
-      // {<div>
-      //   <select id="filter-items" defaultValue="">
-      //     <option value="">Default</option>
-      //     <option value="price_low_to_high">Price, Low to High</option>
-      //     <option value="price_high_to_low">Price, High to Low</option>
-      //     <option value="likes_high_to_low">Most liked</option>
-      //   </select>
-      // </div>}
 
       <div key={collection.id} className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
         style={{ display: "block", backgroundSize: "cover" }}>
@@ -45,7 +34,7 @@ const ExploreItems = ({ nft }) => {
           </div>
 
           <div className="nft__item_wrap">
-            <div className="nft__item_extra">
+            {/* <div className="nft__item_extra">
               <div className="nft__item_buttons">
                 <button>Buy Now</button>
                 <div className="nft__item_share">
@@ -61,7 +50,7 @@ const ExploreItems = ({ nft }) => {
                   </a>
                 </div>
               </div>
-            </div>
+            </div> */}
 
             <Link to="/item-details">
               <img src={collection.nftImage} className="lazy nft__item_preview" alt="" />
@@ -83,14 +72,6 @@ const ExploreItems = ({ nft }) => {
     );
   });
 
-  // {
-  //   {
-      // new Array(8).fill(0).map((_, index) => (
-      // ))
-  //   }
-  // }
-
-
   return (
     <>
       {renderNft}
@@ -104,7 +85,6 @@ const ExploreItems = ({ nft }) => {
       )}
     </>
   );
-
 };
 
 export default ExploreItems;
