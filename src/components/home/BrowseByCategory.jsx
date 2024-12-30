e9aa9f45d011e8452eedf9fde9cef0849ec02b66
tai tran <taitran@sandiego.edu>
2024-12-30 00:00:11 -0800

    added aos library

--- src/components/home/BrowseByCategory.jsx
+++ src/components/home/BrowseByCategory.jsx
@@ -1,51 +1,57 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const BrowseByCategory = () => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <section id="section-category" className="no-top">
       <div className="container">
         <div className="row">
           <div className="col-lg-12">
             <div className="text-center">
-              <h2>Browse by category</h2>
+              <h2 data-aos="fade-up">Browse by category</h2>
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
           <div className="col-md-2 col-sm-4 col-6 mb-sm-30">
             <Link to="/explore" className="icon-box style-2 rounded">
-              <i className="fa fa-image"></i>
-              <span>Art</span>
+              <i className="fa fa-image" data-aos="fade-right"></i>
+              <span data-aos="fade-right">Art</span>
             </Link>
           </div>
           <div className="col-md-2 col-sm-4 col-6 mb-sm-30">
             <Link to="/explore" className="icon-box style-2 rounded">
-              <i className="fa fa-music"></i>
-              <span>Music</span>
+              <i className="fa fa-music" data-aos="fade-right"></i>
+              <span data-aos="fade-right">Music</span>
             </Link>
           </div>
           <div className="col-md-2 col-sm-4 col-6 mb-sm-30">
             <Link to="/explore" className="icon-box style-2 rounded">
-              <i className="fa fa-search"></i>
-              <span>Domain Names</span>
+              <i className="fa fa-search" data-aos="fade-right"></i>
+              <span data-aos="fade-right">Domain Names</span>
             </Link>
           </div>
           <div className="col-md-2 col-sm-4 col-6 mb-sm-30">
             <Link to="/explore" className="icon-box style-2 rounded">
-              <i className="fa fa-globe"></i>
-              <span>Virtual Worlds</span>
+              <i className="fa fa-globe" data-aos="fade-right"></i>
+              <span data-aos="fade-right">Virtual Worlds</span>
             </Link>
           </div>
           <div className="col-md-2 col-sm-4 col-6 mb-sm-30">
             <Link to="/explore" className="icon-box style-2 rounded">
-              <i className="fa fa-vcard"></i>
-              <span>Trading Cards</span>
+              <i className="fa fa-vcard" data-aos="fade-right"></i>
+              <span data-aos="fade-right">Trading Cards</span>
             </Link>
           </div>
           <div className="col-md-2 col-sm-4 col-6 mb-sm-30">
             <Link to="/explore" className="icon-box style-2 rounded">
-              <i className="fa fa-th"></i>
-              <span>Collectibles</span>
+              <i className="fa fa-th" data-aos="fade-right"></i>
+              <span data-aos="fade-right">Collectibles</span>
             </Link>
           </div>
         </div>
--- src/components/home/Landing.jsx
+++ src/components/home/Landing.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NFT from "../../images/nft.png";
 import backgroundImage from "../../images/bg-shape-1.jpg";
 import { Link } from "react-router-dom";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Landing = () => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+  
   return (
     <section
       id="section-hero"
@@ -18,13 +24,13 @@ const Landing = () => {
             <div className="col-md-6">
               <div className="spacer-single"></div>
               <h6>
-                <span className="text-uppercase id-color-2">
+                <span className="text-uppercase id-color-2" data-aos="fade-up">
                   Ultraverse Market
                 </span>
               </h6>
               <div className="spacer-10"></div>
-              <h1>Create, sell or collect digital items.</h1>
-              <p className="lead">
+              <h1 data-aos="fade-up">Create, sell or collect digital items.</h1>
+              <p className="lead" data-aos="fade-up">
                 Unit of data stored on a digital ledger, called a blockchain,
                 that certifies a digital asset to be unique and therefore not
                 interchangeable
@@ -36,7 +42,7 @@ const Landing = () => {
               <div className="mb-sm-30"></div>
             </div>
             <div className="col-md-6 xs-hide">
-              <img src={NFT} className="lazy img-fluid" alt="" />
+              <img src={NFT} className="lazy img-fluid" alt="" data-aos="fade-up" data-aos-duration="3000"/>
             </div>
           </div>
         </div>
